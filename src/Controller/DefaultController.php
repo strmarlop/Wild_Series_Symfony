95d2655c044d81051ed459ffdd65f4e530582e73
Esther Martinez <estmartinezl@gmail.com>
2023-05-23 12:16:43 +0200

    navbar bootstrap, css, lien

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('program/index.html.twig', ['website' => 'Bienvenue',]);
+        return $this->render('index.html.twig', ['website' => 'Bienvenue',]);
     }
 }
