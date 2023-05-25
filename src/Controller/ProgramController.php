88c372f89550768a42dab8d3a253c11fd09d1250
Esther Martinez <estmartinezl@gmail.com>
2023-05-25 09:54:46 +0200

    findAll and findOneBy

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,20 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProgramRepository;
 
 
 #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
-        return $this->render('program/index.html.twig', ['website' => 'Bienvenue',]);
+        $programs = $programRepository->findAll();
+
+        return $this->render('program/index.html.twig', ['programs' => $programs]);
     }
 
-    #[Route('/{id}', requirements: ['id' => '\d+'], methods: ['GET'], name: 'id')]
-    public function show(int $id): Response
+    #[Route('/show/{id<^[0-9]+$>}', name: 'show')]
+    public function show(int $id, ProgramRepository $programRepository): Response
     {
-        return $this->render('program/show.html.twig', ['id' => $id]);
+        $program = $programRepository->findOneBy(['id' => $id]);
+        // same as $program = $programRepository->find($id);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : ' . $id . ' found in program\'s table.'
+            );
+        }
+        return $this->render('program/show.html.twig', ['program' => $program,]);
     }
 }
