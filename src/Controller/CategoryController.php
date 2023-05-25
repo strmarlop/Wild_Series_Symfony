1b37d8d3c66c90b87751605348a91008fe79bee7
Esther Martinez <estmartinezl@gmail.com>
2023-05-25 20:38:35 +0200

    show the programs of one category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
+use Doctrine\ORM\Mapping\OrderBy;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,5 +19,19 @@ class CategoryController extends AbstractController
         $categories = $categoryRepository->findAll();
 
         return $this->render('category/index.html.twig', ['categories' => $categories]);
+    }
+
+    #[Route('/{categoryName}', name: 'show')]
+
+    public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository)
+    {
+        $category = $categoryRepository->findOneByName($categoryName); //prends le champ Name // $category = $categoryRepository->findOneBy(['name' => $categoryName]);
+        if (!$category) {
+            throw $this->createNotFoundException(
+                '404 - No category with name : ' . $categoryName . ' found in category\'s table.'
+            );
+        }
+        $programsByCategory = $programRepository->findByCategory($category->getId(), ['id' => 'DESC'], 3, 0); //id de la categorie
+        return $this->render('category/show.html.twig', ['programsByCategory' => $programsByCategory, 'category' => $category,]);
     }
 }
--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -15,6 +15,10 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
         ['title' => 'Notre planète', 'synopsis' => 'La série présente les espèces les plus précieuses et les habitats les plus fragiles de la planète.', 'category' => 'Documentaire'],
         ['title' => 'American Horror Story', 'synopsis' => 'A chaque saison, son histoire. American Horror Story nous embarque dans des récits à la fois poignants et cauchemardesques, mêlant la peur, le gore et le politiquement correct. De quoi vous confronter à vos plus grandes frayeurs !', 'category' => 'Horreur'],
         ['title' => 'Arcane', 'synopsis' => 'Série animée qui se déroule dans l\'univers de la franchise de jeu vidéo "League of Legends". Intitulée "Arcane", celle-ci raconte les histoires originelles de deux champions emblématiques de la League et le pouvoir qui finit par les déchirer.', 'category' => 'Animation'],
+        ['title' => 'Rick et Morty', 'synopsis' => 'Un brillant inventeur et son petit fils un peu à l\'Ouest partent à l\'aventure...', 'category' => 'Animation'],
+        ['title' => 'Lastman', 'synopsis' => 'Richard Aldana, un jeune boxeur, se retrouve avec la gamine de son meilleur ami sur les bras. Mais la petite Siri est traquée par une secte de fanatiques qui croient à l’existence de la Vallée des Rois, un monde de légendes dont elle serait la clef.', 'category' => 'Animation'],
+        ['title' => 'L\'Attaque des Titans', 'synopsis' => 'Dans un monde ravagé par des titans mangeurs d’homme depuis plus d’un siècle, les rares survivants de l’Humanité n’ont d’autre choix pour survivre que de se barricader dans une cité-forteresse. Le jeune Eren, témoin de la mort de sa mère dévorée par un titan, n’a qu’un rêve : entrer dans le corps d’élite chargé de découvrir l’origine des Titans et les annihiler jusqu’au dernier…', 'category' => 'Animation'],
+
     ];
 
     public function load(ObjectManager $manager)
