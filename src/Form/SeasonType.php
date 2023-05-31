a8365681a66da16767f9f9d81af4c01172f63227
Esther Martinez <estmartinezl@gmail.com>
2023-05-31 16:58:42 +0200

    creation crud for season, and episode

---
+++ src/Form/SeasonType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Season;
+use App\Entity\Program;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class SeasonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('number')
+            ->add('year')
+            ->add('description')
+            ->add('program', EntityType::class, ['class' => Program::class, 'choice_label' =>'title' ]) //en entité program regarder le name de la propriete
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Season::class,
+        ]);
+    }
+}
