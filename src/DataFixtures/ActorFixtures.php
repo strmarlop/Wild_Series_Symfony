f249659063bf2e07ff42c638cf2ed40d382eb646
Esther Martinez <estmartinezl@gmail.com>
2023-06-05 23:00:47 +0200

    link actors in program's page, and link programs in actors page

--- src/DataFixtures/ActorFixtures.php
+++ src/DataFixtures/ActorFixtures.php
@@ -5,26 +5,35 @@ namespace App\DataFixtures;
 use App\Entity\Actor;
 use App\Entity\Program; //
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 
-class ActorFixtures extends Fixture
+class ActorFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         $faker=Factory::create();
 
-        for ($numActor = 0; $numActor <= 10; $numActor++){
-            for ($numProgram = 0; $numProgram <= 3; $numProgram++){
-                $actor=new Actor;
-                $actor->setName($faker->name());
-
-                $program=$this->getReference('program_' . rand(1,7)); //Assignem un dels 7 pgm. I el pgm amb el getreference de l'entité Program
+        for ($numActor = 0; $numActor < 10; $numActor++){
+            $actor=new Actor;
+            $actor->setName($faker->name());
+            for ($numProgram = 0; $numProgram < 3; $numProgram++){ 
+            // var_dump($faker->numberBetween(1, count(ProgramFixtures::PROGRAMS)));
+            // exit();
+                $program=$this->getReference('program_' . $faker->numberBetween(0, count(ProgramFixtures::PROGRAMS)-1)); //entre 0 et num programs-1 
                 $actor->addProgram($program);
-
-                $manager->persist($actor);
             }
+            $manager->persist($actor);
         }  
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        // Tu retournes ici toutes les classes de fixtures dont EpisodeFixtures dépend
+        return [
+            ProgramFixtures::class,
+        ];
     }
 }
