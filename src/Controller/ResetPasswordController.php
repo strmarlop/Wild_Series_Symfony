352d70ab02b9d10fb3fc681b25c82cd5128a575a
Esther Martinez <estmartinezl@gmail.com>
2023-06-16 23:08:08 +0200

    a

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -157,7 +157,7 @@ class ResetPasswordController extends AbstractController
         }
 
         $email = (new TemplatedEmail())
-            ->from(new Address('estmartinezl@gmail.com', 'EstherMail'))
+            ->from(new Address('wilder@wildcodeschool.fr', 'WildCodeSchool'))
             ->to($user->getEmail())
             ->subject('Your password reset request')
             ->htmlTemplate('reset_password/email.html.twig')
